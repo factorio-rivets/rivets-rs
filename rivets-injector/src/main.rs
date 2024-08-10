3071aae08e61653b7ef1cf7c8f05b882b14c37ef
notnotmelon <zacharypicco@gmail.com>
2024-08-09 21:01:25 -0500

    feat: Implement .zip extraction and OS pipes into windows injector script

--- rivets-injector/src/main.rs
+++ rivets-injector/src/main.rs
@@ -1,16 +1,61 @@
 //! A small windows application to inject the Rivets DLL into Factorio.
 
-use anyhow::{anyhow, bail, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use clap::Parser;
 use dll_syringe::process::{BorrowedProcess, ProcessModule};
 use dll_syringe::{process::OwnedProcess, Syringe};
 use std::ffi::CString;
+use std::fs::File;
+use std::io::{self};
+use std::os::windows::io::FromRawHandle;
 use std::path::{Path, PathBuf};
 use windows::core::{PCSTR, PSTR};
-use windows::Win32::Foundation::CloseHandle;
-use windows::Win32::System::Threading::{
-    CreateProcessA, ResumeThread, CREATE_SUSPENDED, PROCESS_INFORMATION, STARTUPINFOA,
+use windows::Win32::Foundation::{
+    CloseHandle, SetHandleInformation, BOOL, HANDLE, HANDLE_FLAG_INHERIT, INVALID_HANDLE_VALUE,
 };
+use windows::Win32::Security::SECURITY_ATTRIBUTES;
+use windows::Win32::System::Pipes::CreatePipe;
+use windows::Win32::System::Threading::{
+    CreateProcessA, ResumeThread, TerminateProcess, WaitForSingleObject, CREATE_SUSPENDED,
+    INFINITE, PROCESS_INFORMATION, STARTUPINFOA,
+};
+use zip::read::ZipArchive;
+
+fn unzip_specific_file(zip_path: &Path, file_name: &str, output_path: &Path) -> Result<()> {
+    let file = File::open(zip_path)?;
+    let mut archive = ZipArchive::new(file).context("Failed to open rivets mod ZIP archive")?;
+
+    let mut zip_file = archive
+        .by_name(file_name)
+        .context("Failed to find rivets.dll in ZIP archive")?;
+
+    let err = format!(
+        "Failed to create output file {}",
+        output_path
+            .to_str()
+            .ok_or(anyhow!("Failed to convert output path to string"))?
+    );
+    let mut output_file = File::create(output_path).context(err)?;
+
+    io::copy(&mut zip_file, &mut output_file)
+        .context("Failed to write ZIP entry to output file")?;
+
+    Ok(())
+}
+
+fn extract_dll(mods_folder: &Path) -> Result<PathBuf> {
+    const DLL_NAME: &str = "rivets/rivets.dll";
+
+    let zip_path = mods_folder.join("rivets_0.0.1.zip");
+    let dll_path = mods_folder.join(DLL_NAME);
+
+    let mut output_path = std::env::current_dir()?;
+    output_path.push("rivets.dll");
+
+    unzip_specific_file(&zip_path, DLL_NAME, &output_path)?;
+
+    Ok(dll_path)
+}
 
 fn get_syringe() -> Result<Syringe> {
     let Some(process) = OwnedProcess::find_first_by_name("factorio") else {
@@ -22,13 +67,29 @@ fn get_syringe() -> Result<Syringe> {
 
 fn inject_dll<'a>(
     syringe: &'a Syringe,
-    dll_name: &Path,
+    dll_path: &Path,
 ) -> Result<ProcessModule<BorrowedProcess<'a>>> {
     println!("Injecting DLL into Factorio process...");
 
     syringe
-        .inject(dll_name)
+        .inject(dll_path)
         .map_err(|e| anyhow!("Failed to inject DLL: {e}"))
+}
+
+fn create_pipe() -> Result<(HANDLE, HANDLE)> {
+    let mut stdout_read = INVALID_HANDLE_VALUE;
+    let mut stdout_write = INVALID_HANDLE_VALUE;
+    let sa = SECURITY_ATTRIBUTES {
+        nLength: std::mem::size_of::<SECURITY_ATTRIBUTES>() as u32,
+        bInheritHandle: BOOL(1), // The child process needs to inherit the handle
+        lpSecurityDescriptor: std::ptr::null_mut(),
+    };
+    unsafe {
+        CreatePipe(&mut stdout_read, &mut stdout_write, Some(&sa), 0)?;
+        SetHandleInformation(stdout_read, 0, HANDLE_FLAG_INHERIT)?;
+    }
+
+    Ok((stdout_read, stdout_write))
 }
 
 fn start_factorio(factorio_path: PCSTR) -> Result<PROCESS_INFORMATION> {
@@ -61,29 +122,52 @@ fn start_factorio(factorio_path: PCSTR) -> Result<PROCESS_INFORMATION> {
 #[derive(Parser, Debug)]
 #[clap(version, about, long_about = None)]
 struct Cli {
-    factorio_path: PathBuf,
-    dll_path: PathBuf,
+    mods_folder: PathBuf,
 }
 
 fn main() -> Result<()> {
+    let (stdout_read, _) = create_pipe()?;
+    let mut reader = unsafe { std::fs::File::from_raw_handle(stdout_read.0) };
+
     let cli = Cli::parse();
 
-    let factorio_path = CString::new(cli.factorio_path.as_os_str().to_string_lossy().into_owned())?;
+    let mut factorio_path = std::env::current_dir()?;
+    factorio_path.push("factorio.exe");
+
+    let factorio_path = CString::new(factorio_path.as_os_str().to_string_lossy().into_owned())?;
     println!("Factorio path: {factorio_path:?}");
     let factorio_path = PCSTR(factorio_path.as_ptr().cast());
 
-    let factorio_process_information: PROCESS_INFORMATION = start_factorio(factorio_path)?;
-    let process_handle = factorio_process_information.hProcess;
+    let dll_path = extract_dll(&cli.mods_folder)?;
 
-    let syringe = get_syringe()?;
-    inject_dll(&syringe, &cli.dll_path)?;
+    let factorio_process_information: PROCESS_INFORMATION = start_factorio(factorio_path)?;
+    println!("Factorio process started.");
+    let syringe = get_syringe().inspect_err(|_| {
+        attempt_kill_factorio(factorio_process_information);
+    })?;
+    inject_dll(&syringe, &dll_path)?;
     println!("DLL injected successfully.");
 
     unsafe {
         ResumeThread(factorio_process_information.hThread);
         CloseHandle(factorio_process_information.hThread).ok();
-        CloseHandle(process_handle).ok();
     }
 
+    // Duplicate the factorio stdout stream onto our own stdout using OS pipes.
+    io::copy(&mut reader, &mut io::stdout())?;
+
+    unsafe {
+        WaitForSingleObject(factorio_process_information.hProcess, INFINITE);
+    }
+
+    println!("Factorio process exited.");
+
     Ok(())
+}
+
+fn attempt_kill_factorio(factorio_process_information: PROCESS_INFORMATION) {
+    if let Err(e) = unsafe { TerminateProcess(factorio_process_information.hProcess, 0) } {
+        eprintln!("Failed to terminate Factorio process after experiencing a rivets error: {e}");
+        eprintln!("You likely have a ghost Factorio process running. Please kill it manually via task manager.");
+    }
 }
